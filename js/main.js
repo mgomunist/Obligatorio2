46c07fd72e110fe913feaac935fbadae6e8fd745
Mariana Gómez <gomunistmariana@gmail.com>
2025-06-29 20:34:30 +0000

    Comentarios

--- js/main.js
+++ js/main.js
@@ -8,10 +8,13 @@ function agregarCorredorDesdeFormulario() {
     let cedula = document.getElementById("cedula").value;
     let ficha = document.getElementById("fecha-medica").value;
 
+    // Obtener el tipo de corredor desde el grupo de radios
     let tipo = document.querySelector('input[name="tipo-corredor"]:checked').value;
 
+    // Crear el corredor
     let corredor = new Corredor(nombre, edad, cedula, ficha, tipo);
 
+    // Agregar al sistema
     sistema.corredores.push(corredor);
 }
 
